dfdadccce4e17aef43841bc1ed5f595c90ee7979
VM <vmaksimovic.cr34@gmail.com>
2021-02-15 22:51:40 +0100

    creating component

---
+++ bt-react-2021/src/components/Header.js
@@ -0,0 +1,13 @@
+//import React library
+import React from 'react'
+
+// react f-n component
+const Header = () => {
+  // return JSX
+  <header>
+    <h2>Header Component</h2>
+  </header>
+}
+
+// export component
+export default Header

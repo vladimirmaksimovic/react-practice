80d73ae51f7d4b6b3dfe0a2d02efac72c9b48b5e
vladimirmaksimovic <vmaksimovic.cr34@gmail.com>
2021-02-16 17:53:46 +0100

    props and prop types

--- bt-react-2021/src/App.js
+++ bt-react-2021/src/App.js
@@ -1,11 +1,15 @@
-//import React library
+// import React library
 import React from 'react'
+import Header from './components/Header';
 
 // react f-n component
 function App() {
+  // return JSX
   return (
+    // JSX expression must have only one parent element
     <div className="App">
       <h1>React App</h1>
+      <Header />
     </div>
   );
 }
--- bt-react-2021/src/components/Header.js
+++ bt-react-2021/src/components/Header.js
@@ -1,12 +1,35 @@
 //import React library
 import React from 'react'
+// import Prop Types library
+import PropTypes from 'prop-types'
 
-// react f-n component
-const Header = () => {
+// react f-n component, without props object i.e. with destructuring
+const Header = ({ title }) => {
   // return JSX
+  return (
   <header>
-    <h2>Header Component</h2>
+    <h2>{ title }</h2>
   </header>
+  )
+}
+
+// component with props object
+/* const Header = (props) => {
+  return (
+  <header>
+    <h2>{props.title}</h2>
+  </header>
+  )
+} */
+
+// default props
+Header.defaultProps = {
+  title: 'Task Tracker'
+}
+
+// set prop Types
+Header.propTypes = {
+  title: PropTypes.string,
 }
 
 // export component

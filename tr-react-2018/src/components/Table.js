72f9e10423985e6301bfc9c868d52f0b4564876a
VM <vmaksimovic.cr34@gmail.com>
2021-02-27 22:45:37 +0100

    simple and class components

--- tr-react-2018/src/components/Table.js
+++ tr-react-2018/src/components/Table.js
@@ -1,33 +1,52 @@
+// import React library
 import React, { Component } from 'react'
 
-export class Table extends Component {
+// f-n components / simple components
+const TableHeader = () => {
+  //return JSX
+  return (
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Job</th>
+      </tr>
+    </thead>
+  )
+}
+
+const TableBody = () => {
+  return (
+    <tbody>
+      <tr>
+        <td>Natasa</td>
+        <td>Sekretar</td>
+      </tr>
+      <tr>
+        <td>Branka</td>
+        <td>Racunovodja</td>
+      </tr>
+      <tr>
+        <td>Suza</td>
+        <td>Administracija</td>
+      </tr>
+      <tr>
+        <td>Dragana</td>
+        <td>Direktor</td>
+      </tr>
+    </tbody>
+  )
+}
+
+// class component
+class Table extends Component {
+  // render method
   render() {
+    // return JSX
     return (
+      // JSX
       <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Job</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Natasa</td>
-            <td>Sekretar</td>
-          </tr>
-          <tr>
-            <td>Branka</td>
-            <td>Racunovodja</td>
-          </tr>
-          <tr>
-            <td>Suza</td>
-            <td>Administracija</td>
-          </tr>
-          <tr>
-            <td>Dragana</td>
-            <td>Direktor</td>
-          </tr>
-        </tbody>
+        <TableHeader />
+        <TableBody />
       </table>
     )
   }
